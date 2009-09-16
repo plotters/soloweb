19ac6e7fe34e768031093fc880c60bc830202597
hugith@gmail.com <hugith@gmail.com@f566008a-8cac-11de-b0b7-87b715de9b2a>
2009-09-16 11:27:28 +0000

    Moving selectedPage() functionality up to SWAbstractComponent

--- trunk/USSoloWeb29/Sources/is/us/soloweb/SWGenericSiteLook.java
+++ trunk/USSoloWeb29/Sources/is/us/soloweb/SWGenericSiteLook.java
@@ -1,13 +1,6 @@
 package is.us.soloweb;
 
-import is.us.soloweb.data.SWPage;
-import is.us.soloweb.util.SWPageUtilities;
-
 import com.webobjects.appserver.WOContext;
-import com.webobjects.eocontrol.EOEditingContext;
-
-import er.extensions.components.ERXComponent;
-import er.extensions.eof.ERXEC;
 
 /**
  * SWGenericSiteLook is the common ancestor of all Site Looks created for SoloWeb.
@@ -18,44 +11,9 @@ import er.extensions.eof.ERXEC;
  * @since 2.3
  */
 
-public abstract class SWGenericSiteLook extends ERXComponent {
-
-	/**
-	 * The currently selected page.
-	 */
-	private SWPage _selectedPage;
-
-	private EOEditingContext _ec;
+public abstract class SWGenericSiteLook extends SWAbstractComponent {
 
 	public SWGenericSiteLook( WOContext context ) {
 		super( context );
-	}
-
-	/**
-	 * The editingContext
-	 */
-	private EOEditingContext ec() {
-		if( _ec == null ) {
-			_ec = ERXEC.newEditingContext();
-		}
-
-		return _ec;
-	}
-
-	/**
-	 * Returns the selected page.
-	 */
-	public SWPage selectedPage() {
-		if( _selectedPage == null )
-			_selectedPage = SWPageUtilities.pageFromRequest( ec(), context().request() );
-
-		return _selectedPage;
-	}
-
-	/**
-	 * Sets the currently selected page.
-	 */
-	public void setSelectedPage( SWPage p ) {
-		_selectedPage = p;
 	}
 }
