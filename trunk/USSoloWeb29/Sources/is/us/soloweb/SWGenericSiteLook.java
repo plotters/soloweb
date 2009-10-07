6715eb4d7d2374faa4bff4a7bf02e7a463117a5f
hugith@gmail.com <hugith@gmail.com@f566008a-8cac-11de-b0b7-87b715de9b2a>
2009-10-07 11:15:05 +0000

    Added support for showing the page title in SWGenericSiteLook

--- trunk/USSoloWeb29/Sources/is/us/soloweb/SWGenericSiteLook.java
+++ trunk/USSoloWeb29/Sources/is/us/soloweb/SWGenericSiteLook.java
@@ -1,6 +1,11 @@
 package is.us.soloweb;
 
+import is.us.soloweb.data.SWNewsItem;
+import is.us.util.*;
+
 import com.webobjects.appserver.WOContext;
+
+import er.extensions.eof.ERXEC;
 
 /**
  * SWGenericSiteLook is the common ancestor of all Site Looks created for SoloWeb.
@@ -15,5 +20,45 @@ public abstract class SWGenericSiteLook extends SWAbstractComponent {
 
 	public SWGenericSiteLook( WOContext context ) {
 		super( context );
+	}
+
+	/**
+	 * Name of the given page.
+	 */
+	public String pageTitle() {
+		SWNewsItem newsItem = selectedItem();
+
+		if( newsItem != null ) {
+			return newsItem.name();
+		}
+
+		return selectedPage().nameWithPrefix();
+	}
+
+	/**
+	 * Meta description of the page.
+	 */
+	public String pageDescription() {
+		SWNewsItem newsItem = selectedItem();
+
+		if( newsItem != null ) {
+			return selectedItem().excerpt();
+		}
+
+		return selectedPage().text();
+	}
+
+	/**
+	 * The currently selected newsitem.  
+	 */
+	private SWNewsItem selectedItem() {
+		String newsitemIDString = context().request().stringFormValueForKey( "detail" );
+
+		if( USStringUtilities.stringHasValue( newsitemIDString ) ) {
+			Integer newsitemID = USUtilities.integerFromObject( newsitemIDString );
+			return SWNewsItem.fetchSWNewsItem( ERXEC.newEditingContext(), SWNewsItem.NEWS_ITEM_ID.eq( newsitemID ) );
+		}
+
+		return null;
 	}
 }
