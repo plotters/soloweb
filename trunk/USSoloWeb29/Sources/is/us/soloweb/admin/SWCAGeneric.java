9c1a9ec16ceb015c04058174cbefba4b01970426
hugith@gmail.com <hugith@gmail.com@f566008a-8cac-11de-b0b7-87b715de9b2a>
2009-10-22 08:41:30 +0000

    Added a generic component inspector

---
+++ trunk/USSoloWeb29/Sources/is/us/soloweb/admin/SWCAGeneric.java
@@ -0,0 +1,16 @@
+package is.us.soloweb.admin;
+
+import is.us.soloweb.SWGenericComponent;
+
+import com.webobjects.appserver.WOContext;
+
+/**
+ * @author Hugi Þórðarson
+ */
+
+public class SWCAGeneric extends SWGenericComponent {
+
+	public SWCAGeneric( WOContext context ) {
+		super( context );
+	}
+}
