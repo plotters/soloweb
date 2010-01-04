9a1dbd51cb5212721429c7da28f61ca1746fe172
hugith@gmail.com <hugith@gmail.com@f566008a-8cac-11de-b0b7-87b715de9b2a>
2010-01-04 13:13:53 +0000

    VEF-554 : Fixed sort ordering of form folders

--- USSoloForms29/Sources/is/us/soloweb/forms/data/SWFFormFolder.java
+++ USSoloForms29/Sources/is/us/soloweb/forms/data/SWFFormFolder.java
@@ -1,10 +1,15 @@
 package is.us.soloweb.forms.data;
 
+import is.us.soloweb.data.SWDocument;
 import is.us.soloweb.forms.data.auto._SWFFormFolder;
-import is.us.soloweb.interfaces.*;
+import is.us.soloweb.interfaces.SWFolder;
+import is.us.soloweb.interfaces.SWInheritsPrivileges;
+import is.us.soloweb.interfaces.SWInspectable;
 import is.us.soloweb.util.SWFolderUtilities;
+import is.us.util.USArrayUtilities;
 
-import com.webobjects.eocontrol.*;
+import com.webobjects.eocontrol.EOEditingContext;
+import com.webobjects.eocontrol.EOEnterpriseObject;
 import com.webobjects.foundation.NSArray;
 
 /**
@@ -13,36 +18,45 @@ import com.webobjects.foundation.NSArray;
 
 public class SWFFormFolder extends _SWFFormFolder implements SWFolder<SWFFormFolder>, SWInspectable {
 
+	@Override
 	public NSArray<SWFForm> sortedDocuments() {
-		return SWFolderUtilities.sortedDocuments( this );
+		return USArrayUtilities.sortedArrayUsingIcelandicComparator( documents(), SWDocument.NAME_KEY );
+
 	}
 
+	@Override
 	public void transferOwnership( EOEnterpriseObject newOwner ) {
 		this.removeObjectFromBothSidesOfRelationshipWithKey( parent(), PARENT_KEY );
 		this.addObjectToBothSidesOfRelationshipWithKey( newOwner, PARENT_KEY );
 	}
 
+	@Override
 	public NSArray<SWFFormFolder> sortedRootFolders( EOEditingContext ec ) {
 		return SWFolderUtilities.sortedRootFolders( ec, this );
 	}
 
+	@Override
 	public NSArray<SWFFormFolder> sortedSubFolders() {
 		return SWFolderUtilities.sortedSubFolders( this );
 	}
 
-	public int size() {
+	@Override
+	public long size() {
 		return SWFolderUtilities.size( this );
 	}
 
+	@Override
+	public double sizeKB() {
+		return SWFolderUtilities.sizeKB( this );
+	}
+
+	@Override
 	public int count() {
 		return SWFolderUtilities.count( this );
 	}
 
+	@Override
 	public SWInheritsPrivileges inheritsPrivilegesFrom() {
 		return parent();
-	}
-
-	public NSArray<EOSortOrdering> itemSordOrderings() {
-		return SWFForm.NAME.ascs();
 	}
 }
